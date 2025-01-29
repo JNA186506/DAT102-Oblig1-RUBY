94197db19cfd02468fdba5d5a55c454e18b426f1
JohannesNA <186506@stud.hvl.no>
2025-01-29 13:16:11 +0100

    Tidsmåling ferdig

--- Tidsmaaling/Main.java
+++ Tidsmaaling/Main.java
@@ -1,27 +1,46 @@
 package Tidsmaaling;
 
 import static java.lang.System.currentTimeMillis;
+import static java.lang.System.nanoTime;
 
 public class Main {
 
     public static void main(String[] args) {
-        Tidsmaaling tid = new Tidsmaaling();
+        Tidsmaaling t = new Tidsmaaling();
 
-        long tidsBruk = currentTimeMillis();
-        tid.tid((long) Math.pow(10,7));
-        long tidsBruk2 = currentTimeMillis() - tidsBruk;
-        System.out.println(tid.beregnGjnstt(tidsBruk2) + " ms");
+        long tidsBruk = 0;
+        tidsBruk = nanoTime();
 
-        tidsBruk = currentTimeMillis();
-        tid.tid((long) Math.pow(10,8));
-        tidsBruk2 = currentTimeMillis() - tidsBruk;
-        System.out.println(tid.beregnGjnstt(tidsBruk2) + " ms");
+        for (int i = 0; i < 10; i++) {
+            t.tid((long) Math.pow(10,7));
+        }
 
+        System.out.println("Tidsmåling 1, 10^7 : " + (nanoTime() - tidsBruk)/10 + " nanosekund");
 
-        tidsBruk = currentTimeMillis();
-        tid.tid((long) Math.pow(10,9));
-        tidsBruk2 = currentTimeMillis() - tidsBruk;
-        System.out.println(tid.beregnGjnstt(tidsBruk2) + " ms");
+        tidsBruk = nanoTime();
+
+        for (int i = 0; i < 10; i++) {
+            t.tid((long) Math.pow(10,8));
+        }
+
+        System.out.println("Tidsmåling 2, 10^8 : " + (nanoTime() - tidsBruk)/10 + " nanosekund");
+
+        tidsBruk = nanoTime();
+
+        for (int i = 0; i < 10; i++) {
+            t.tid((long) Math.pow(10,9));
+        }
+
+        System.out.println("Tidsmåling 3, 10^9 : " + (nanoTime() - tidsBruk)/10 + " nanosekund");
+
+        tidsBruk = nanoTime();
+
+        for (int i = 0; i < 10; i++) {
+            t.tid((long) Math.pow(10,10));
+        }
+
+        System.out.println("Tidsmåling 4, 10^10: " + (nanoTime() - tidsBruk)/10 + " nanosekund");
+
     }
 
 }
--- Tidsmaaling/Tidsmaaling.java
+++ Tidsmaaling/Tidsmaaling.java
@@ -8,19 +8,7 @@ public class Tidsmaaling {
         for (long i = 1; i <= n; i++) {
             k = k + 5;
         }
-// …fyll ut
-    }
-//
-//    public long convertTid(long n) {
-//        return n / 1000;
-//    }
 
-    public int beregnGjnstt(long n) {
-        int sum = 0;
-        for (int i = 0; i < 10; i++) {
-            sum += (int) n;
-        }
-        return sum / 10;
     }
 
 }
